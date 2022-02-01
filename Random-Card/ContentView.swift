ee7a86e7d4929bb3227c3e220e42a5bd04b495d7
Conor Howard <47044266+conorhoward6510@users.noreply.github.com>
2022-02-01 23:17:06 +0000

    Rename Card struct to PlayingCard and move to PlayingCard.swift file

--- Random-Card/ContentView.swift
+++ Random-Card/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var currentCard = Card()
+    @State var currentCard = PlayingCard()
     
     var body: some View {
         VStack {
@@ -23,7 +23,7 @@ struct ContentView: View {
             Spacer()
             
             Button {
-                currentCard = Card()
+                currentCard = PlayingCard()
             } label: {
                 Text("Draw Card")
                     .font(.title2)
@@ -39,53 +39,6 @@ struct ContentView: View {
 
         }
         .padding(.horizontal, 40)
-    }
-    
-}
-
-struct Card {
-    
-    let cardNumber = Int.random(in: 0..<51)
-    var cardImageName: String {
-        return "\(getCardValue())_of_\(getCardSuit())"
-    }
-    
-    func getCardSuit() -> String {
-        
-        switch cardNumber % 4 {
-        case 0:
-            return "clubs"
-        case 1:
-            return "diamonds"
-        case 2:
-            return "hearts"
-        case 3:
-            return "spades"
-        default:
-            fatalError()
-        }
-        
-    }
-    
-    func getCardValue() -> String {
-        
-        let value = cardNumber % 13
-        
-        switch value {
-        case 0:
-            return "ace"
-        case 1...9:
-            return String(value + 1)
-        case 10:
-            return "jack"
-        case 11:
-            return "queen"
-        case 12:
-            return "king"
-        default:
-            fatalError()
-        }
-        
     }
     
 }
---
+++ Random-Card/PlayingCard.swift
@@ -0,0 +1,55 @@
+//
+//  PlayingCard.swift
+//  Random-Card
+//
+//  Created by Conor Howard on 01/02/2022.
+//
+
+import Foundation
+
+struct PlayingCard {
+    
+    let cardNumber = Int.random(in: 0..<51)
+    var cardImageName: String {
+        return "\(getCardValue())_of_\(getCardSuit())"
+    }
+    
+    func getCardSuit() -> String {
+        
+        switch cardNumber % 4 {
+        case 0:
+            return "clubs"
+        case 1:
+            return "diamonds"
+        case 2:
+            return "hearts"
+        case 3:
+            return "spades"
+        default:
+            fatalError()
+        }
+        
+    }
+    
+    func getCardValue() -> String {
+        
+        let value = cardNumber % 13
+        
+        switch value {
+        case 0:
+            return "ace"
+        case 1...9:
+            return String(value + 1)
+        case 10:
+            return "jack"
+        case 11:
+            return "queen"
+        case 12:
+            return "king"
+        default:
+            fatalError()
+        }
+        
+    }
+    
+}
