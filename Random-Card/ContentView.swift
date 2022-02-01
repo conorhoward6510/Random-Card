e20da3d74f9142a7af322160f8b7f5c5f9573834
Conor Howard <47044266+conorhoward6510@users.noreply.github.com>
2022-02-01 23:10:04 +0000

    completed app functionality

--- Random-Card/ContentView.swift
+++ Random-Card/ContentView.swift
@@ -8,14 +8,94 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var currentCard = Card()
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            
+            Spacer()
+            
+            Image(currentCard.cardImageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            Spacer()
+            
+            Button {
+                currentCard = Card()
+            } label: {
+                Text("Draw Card")
+                    .font(.title2)
+                    .fontWeight(.bold)
+            }
             .padding()
+            .padding(.horizontal, 75)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Capsule())
+            
+            Spacer()
+
+        }
+        .padding(.horizontal, 40)
     }
+    
+}
+
+struct Card {
+    
+    let cardNumber = Int.random(in: 0..<51)
+    var cardImageName: String {
+        return "\(getCardValue())_of_\(getCardSuit())"
+    }
+    
+    func getCardSuit() -> String {
+        
+        switch cardNumber % 4 {
+        case 0:
+            return "clubs"
+        case 1:
+            return "diamonds"
+        case 2:
+            return "hearts"
+        case 3:
+            return "spades"
+        default:
+            fatalError()
+        }
+        
+    }
+    
+    func getCardValue() -> String {
+        
+        let value = cardNumber % 13
+        
+        switch value {
+        case 0:
+            return "ace"
+        case 1...9:
+            return String(value + 1)
+        case 10:
+            return "jack"
+        case 11:
+            return "queen"
+        case 12:
+            return "king"
+        default:
+            fatalError()
+        }
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
     }
 }
