db0e7be836085ae83af02973953d65c4e6f7ddfb
Conor Howard <47044266+conorhoward6510@users.noreply.github.com>
2022-02-01 22:39:59 +0000

    Initial Commit

---
+++ Random-Card/Random_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Random_CardApp.swift
+//  Random-Card
+//
+//  Created by Conor Howard on 01/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Random_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
